5897fb5844dcec09727e80354a20a749d261d02e
git <zzh1989@126.com>
2018-12-10 16:09:26 +0800

    todolistComponent

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
 <script>
+import TodoList from './components/TodoList'
+
 export default {
-  name: 'App'
+  components:{
+    TodoList,
+  },
+  data(){
+    return {
+      todos:[
+        {
+          title:'Todo A',
+          project:'Project A',
+          done:false
+        },
+        {
+          title:'Todo B',
+          project:'Project B',
+          done:true
+        },
+        {
+          title:'Todo C',
+          project:'Project C',
+          done:false
+        },
+        {
+          title:'Todo D',
+          project:'Project D',
+          done:true
+        },
+      ],
+    };
+  },
 }
 </script>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>completed tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>pending tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+
+    <div class='ui centered card' v-for='todo in todos'>
+        <div class="content">
+            <div class="header">
+                {{ todo.title }}
+            </div>
+            <div class="meta">
+                {{ todo.project }}
+            </div>
+            <div class="extra content">
+                <span class="right floated edit icon">
+                    <i class="edit icon"></i>
+                </span>
+            </div>
+        </div>
+        <div class="ui bottom attached green basic button" v-show="todo.done">
+            completed
+        </div>
+        <div class="ui bottom attached red basic button" v-show="!todo.donw">
+            complete
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['todos'],
+}
+</script>
+
+
+<style>
+
+</style>
