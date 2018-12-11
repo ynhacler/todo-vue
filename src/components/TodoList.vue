4c12085623d8cbb1a6e4f97b227fa58b1d93db1d
git <zzh1989@126.com>
2018-12-11 14:30:21 +0800

    add delete todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,33 +3,26 @@
     <p>completed tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>pending tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
 
-    <div class='ui centered card' v-for='todo in todos'>
-        <div class="content">
-            <div class="header">
-                {{ todo.title }}
-            </div>
-            <div class="meta">
-                {{ todo.project }}
-            </div>
-            <div class="extra content">
-                <span class="right floated edit icon">
-                    <i class="edit icon"></i>
-                </span>
-            </div>
-        </div>
-        <div class="ui bottom attached green basic button" v-show="todo.done">
-            completed
-        </div>
-        <div class="ui bottom attached red basic button" v-show="!todo.donw">
-            complete
-        </div>
-    </div>
+    <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo">
+
+    </todo>
   </div>
 </template>
 
 <script>
+import Todo from './Todo';
+
 export default {
   props:['todos'],
+  components:{
+      Todo,
+  },
+  methods:{
+      deleteTodo(todo){
+          const todoIndex = this.todos.indexOf(todo);
+          this.todos.splice(todoIndex,1);
+      },
+  },
 }
 </script>
 
